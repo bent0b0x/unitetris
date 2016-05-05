d3ffe8894063bdb359e71055859deb5ec3e6f93b
Benjamin Baum <bjb253@cornell.edu>
2016-05-04 21:46:27 -0500

    (fix) add logic to protect against false corner collisions between pieces (resolves #6)

--- Assets/Scripts/PieceManager.cs
+++ Assets/Scripts/PieceManager.cs
@@ -6,14 +6,16 @@ public class PieceManager : MonoBehaviour {
 	public float width = 2.0f;
 	public float height;
 	public WaitForSeconds moveWait = new WaitForSeconds(1.0f);
+	[HideInInspector] public bool stationary = false;
 
-	// Use this for initialization
 
-		
+	Rigidbody rb;
 
-	
-	// Update is called once per frame
-	void Update () {
+
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody> ();}
+
+		void Update () {
 	}
 
 	public void BeginMotion () {
@@ -22,11 +24,39 @@ public class PieceManager : MonoBehaviour {
 
 	IEnumerator Move () {
 		yield return moveWait;
-		transform.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
-		yield return Move ();
+		if (!stationary) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y - 1, transform.position.z);
+			yield return Move ();
+		}
 	}
 
 	void OnTriggerEnter(Collider other) {
-		Debug.Log (other.ToString());
+		if (other.CompareTag ("Floor")) {
+			stationary = true;
+			return;
+		}
+		if (other.CompareTag ("PieceComponent")) {
+
+			// Raycast from the other collider downwards.
+			// If we hit this game piece, then we know we need to stop moving.
+			// This check is necessary to avoid false collision on corners.
+			Vector3 rayOrigin = other.bounds.center;
+			Vector3 direction = new Vector3 (0f, -1.0f, 0f);
+			Ray pieceRay = new Ray (rayOrigin, direction);
+
+			RaycastHit pieceHit;
+
+			if (Physics.Raycast (pieceRay, out pieceHit, 1.0f)) {
+				if (pieceHit.rigidbody != null && pieceHit.rigidbody.Equals(rb)) {
+					stationary = true;
+					PieceManager otherManager = other.gameObject.GetComponentInParent<PieceManager> ();
+					if (otherManager != null) {
+						otherManager.stationary = true;
+					}
+					return;
+				}
+			}
+		}
 	}
+
 }
